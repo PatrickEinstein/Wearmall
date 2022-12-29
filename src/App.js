9868ddc303c061294979cfbfd72befb600faa275
PatrickEinstein <mohammedola1234@gmai.com>
2022-12-29 05:18:36 -0700

    added headers

--- src/App.js
+++ src/App.js
@@ -4,21 +4,40 @@ import Homepage from './pages/Homepage/homepage';
 import ShopPage from './pages/ShopPage/shop-page';
 import { BrowserRouter, Routes, Route, Link, Switch } from 'react-router-dom';
 import Header from './components/header/header';
+import { ReactComponent as Logo} from './assets/assets.svg';
+
 
 function App() {
   return (
-    <div>
-      < Header/>
-      <BrowserRouter>
-        <Routes>
+
+    <BrowserRouter>
+    <div className='header'>
+      <div className='logo-container'>
+        <Link to="/"> <Logo className='logo' /> </Link>
+        </div>
+        
+        <div>
+            <Link to="/shop"> SHOP </Link>
+            
+            <Link to="/contact"> CONTACT </Link>
+        </div>
+
+        
+    </div>
+
+
+
+        
+      <Routes>
           <Route exact path='/' element={<Homepage />} />
           <Route exact path='/shop' element={<ShopPage/>} />
          </Routes>
       </BrowserRouter>
+        
           
         
      
-    </div>
+  
   );
 }
 
