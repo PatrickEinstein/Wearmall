0c0891629e16a8a14c8d8e3a7febc0436b56b3fb
Your Name <mohammedola1234@gmail.com>
2023-02-03 00:44:53 +0100

    added checkout page and incorporated increment and decrement button

--- src/components/checkout-item/checkout-item.jsx
+++ src/components/checkout-item/checkout-item.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import './checkout-item.scss';
+import { connect, Connect } from "react-redux";
+import { clearItemFromCart, addItem, removeItem } from "../../redux/actions/cart-action";
 
-const CheckOutItem = ( {cartItem :{ name, imageUrl, price, quantity }}) => {
+const CheckOutItem = ( {cartItem , clearItem , addItem, removeItem}) => {
+    
+    const { name, imageUrl, price, quantity } = cartItem;
     return(
     <div className="checkout-item">
         <div className="image-container">
@@ -9,15 +13,26 @@ const CheckOutItem = ( {cartItem :{ name, imageUrl, price, quantity }}) => {
         </div>
 
         <span className="name">{name}</span>
-        <span className="quantity">{quantity}</span>
+        <span className="quantity">
+            <div className="arrow" onClick={( ) =>(removeItem( cartItem) )}>&#10094; </div>
+            <span className="value">{quantity}</span>
+            <div className="arrow" onClick={ ( ) => ( addItem( cartItem) )} >&#10095;</div>
+            </span>
 
         <span className="price">{price}</span>
 
-        <div className="remove-button"> &#10005;</div>
+        <div className="remove-button" onClick={() => ( clearItem (cartItem))}> &#10005;</div>
 
 
     </div>
     )
 }
 
-export default CheckOutItem;
+
+const mapDispatchToProps = (dispatch) => ({
+clearItem : ( item ) => (dispatch (clearItemFromCart(item)) ),
+addItem: (item ) => (dispatch (addItem(item))),
+removeItem: ( item ) => (dispatch(removeItem(item)) )
+});
+
+export default connect(null, mapDispatchToProps) (CheckOutItem);
